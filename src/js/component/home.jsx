2fe603e9bff8d570947824194ca7f8b86b0007b6
Inés Sanz <inessanzm@hotmail.es>
2024-10-26 20:54:23 +0000

    feat:first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import  rojo from "../../img/rojo.png";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState('')
+
+	const changeColor = (col) =>{
+		setColor(col)
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+		<div className="d-flex flex-column align-items-center justify-content-center bg-dark">
+			<div onClick={()=> (color!= 'rojo') ? changeColor('rojo') : changeColor(' ') } className={color==='rojo' ? 'iluminao rojo my-2': 'rojo my-2'} ></div>
+			<img 
+					src={rojo} 
+					onClick={() => (color !== 'rojo') ? changeColor('rojo') : changeColor('')}
+					className={color === 'rojo' ? 'rojo__light' : 'rojo__none'}
+				/>
+			<div onClick={()=> (color!= 'naranja') ? changeColor('naranja') : changeColor(' ')} className={color==='naranja' ? 'iluminao naranja my-2': 'naranja my-2'}></div>
+			<div onClick={()=> (color!= 'verde') ? changeColor('verde') : changeColor(' ')} className={color==='verde' ? 'iluminao verde my-2': 'verde my-2'}></div>
+	
+		</div>
 		</div>
 	);
 };
