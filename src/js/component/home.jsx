b0b35c5eabbe74a985b59df4535e4854a6ecff5c
Inés Sanz <inessanzm@hotmail.es>
2025-01-22 10:10:38 +0000

    feat: cursor modified

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import  lightBg from "../../img/light.png";
 import  darkBg from "../../img/dark.png";
 import  cursor from "../../img/cursor.png";
+import  pointer from "../../img/pointer.png";
 
 //create your first component
 const Home = () => {
@@ -35,17 +36,17 @@ const Home = () => {
 	
 
 	return (
-		<div className="container-fluid m-0 p-0 myContainer" >
+		<div className="container-fluid m-0 p-0 myContainer "  style={{ cursor: `url(${cursor})16 16, auto` }} >
 		<div className="d-flex flex-column align-items-center justify-content-center">
-			<div onClick={()=> (color!= 'red') ? changeColor('red') : changeColor(' ')} className="red"></div>
+			<div onClick={()=> (color!= 'red') ? changeColor('red') : changeColor(' ')} className="red" style={{ cursor: `url(${pointer})16 16, auto` }}></div>
 			<div className={color==='red' ? 'r-light': ''} ></div>
-			<div onClick={()=> (color!= 'orange') ? changeColor('orange') : changeColor(' ')} className='orange'></div>
+			<div onClick={()=> (color!= 'orange') ? changeColor('orange') : changeColor(' ')} className='orange' style={{ cursor: `url(${pointer})16 16, auto` }}></div>
 			<div className={color==='orange' ? 'o-light': ''} ></div>
-			<div onClick={()=> (color!= 'green') ? changeColor('green') : changeColor(' ')} className='green'></div>
+			<div onClick={()=> (color!= 'green') ? changeColor('green') : changeColor(' ')} className='green' style={{ cursor: `url(${pointer})16 16, auto` }}></div>
 			<div className={color==='green' ? 'g-light': ''} ></div>
 			<div className="dark"></div>
-			<button className="myMode" onClick={changeMode}>{mode === 'light' ? 'Dark Mode' : 'Light Mode'}</button>
-			<button className="myButton" onClick={randomLights}>Random Lights</button>
+			<button className="myMode" onClick={changeMode} style={{ cursor: `url(${pointer})16 16, auto` }}>{mode === 'light' ? 'Dark Mode' : 'Light Mode'}</button>
+			<button className="myButton" onClick={randomLights} style={{ cursor: `url(${pointer})16 16, auto` }}>Random Lights</button>
 	<img src={lightBg} alt="background" className={mode==='light' ? 'myBgLight': 'myBgDark'}/>
 	<img src={darkBg} alt="background" className="darkBg"/>
 		</div>
